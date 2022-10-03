ba5b27e18e03d24d635e19b5483b97ec8957196c
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2022-10-03 14:34:21 -0500

    Update [...slug].tsx

--- pages/Docs/[...slug].tsx
+++ pages/Docs/[...slug].tsx
@@ -19,6 +19,22 @@ function Doc() {
         });
     }
   }, [router]);
+  
+  if(!data){
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+        <h1 className="rounded-md bg-primary-300/20 p-4 text-9xl font-black text-primary-500">
+          404
+        </h1>
+        <div className="text-center text-xl">
+          <p>
+            Looks like the documentation for {router.query.slug} is nowhere to be
+            found!
+          </p>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
