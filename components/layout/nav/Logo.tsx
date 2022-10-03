137c4af43b8349671f13ff21dc4f4e99abc9c4eb
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2022-10-03 15:53:14 -0500

    Fix Logo if no image provided
    Ex. Logo is a wordmark so no image file is needed.

--- components/layout/nav/Logo.tsx
+++ components/layout/nav/Logo.tsx
@@ -1,26 +1,33 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 const prefix = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
+const customLoader = ({ src }: any) => {
+  return src;
+};
+
 const Logo = () => {
-  const customLoader = ({ src }: any) => {
-    return src;
-  };
+  const [fallbackImage, setFallbackImage] = useState(false);
 
   return (
     <Link href="/" passHref>
       <a className="flex h-full cursor-pointer items-center justify-start gap-2 text-xl font-black">
-        <span className="relative h-10 w-24 overflow-hidden">
-          <Image
-            loader={customLoader}
-            src={`${prefix}/static/logo.svg`}
-            alt="Profile Image"
-            width="100%"
-            height="100%"
-            layout="fill"
-            className="cursor-pointer"
-          />
-        </span>
+        {!fallbackImage &&
+          <span className="relative h-10 w-24 overflow-hidden">
+            <Image
+              loader={customLoader}
+              src={`${prefix}/static/logo.svg`}
+              alt="Profile Image"
+              width="100%"
+              height="100%"
+              layout="fill"
+              onError={() => {
+                setFallbackImage(true);
+              }}
+            />
+          </span>
+        }
         <span className="hidden md:inline-block">
           {process.env.NEXT_PUBLIC_PROJECT_NAME}
         </span>
