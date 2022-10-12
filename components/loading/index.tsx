f6f83659d6be78e8665e21016a5b200c52b1cb6a
dishwasher-detergent <kenny_bass@outlook.com>
2022-10-12 18:14:47 -0500

    Update index.tsx

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -3,7 +3,7 @@ const Loading = () => {
     <div className="flex h-full w-full items-center justify-center">
       <svg
         aria-hidden="true"
-        className="h-8 w-8 animate-spin fill-yellow-500 text-gray-200 dark:text-gray-600"
+        className="h-8 w-8 animate-spin fill-primary-500 text-gray-200 dark:text-gray-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
