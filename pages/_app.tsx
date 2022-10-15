5ae49f60840ced396ef1519158a367cf3f798145
dishwasher-detergent <kenny_bass@outlook.com>
2022-10-14 22:09:12 -0500

    seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { DefaultSeo } from "next-seo";
-import seoConfig from "../next-seo.config";
+import { NextSeo } from "next-seo";
 import { SidebarProvider } from "../context/SidebarContext";
 import { DarkmodeProvider } from "../context/DarkModeContext";
 import Layout from "../components/layout/Layout";
@@ -9,7 +8,34 @@ import Layout from "../components/layout/Layout";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <DefaultSeo {...seoConfig} />
+      <NextSeo
+        title={`${process.env.NEXT_PUBLIC_PROJECT_NAME}`}
+        canonical={`${process.env.NEXT_PUBLIC_PRODUCTION_ROOT_URL}`}
+        description={`Documentation for ${process.env.NEXT_PUBLIC_PROJECT_NAME}`}
+        openGraph={{
+          title: `${process.env.NEXT_PUBLIC_PROJECT_NAME}`,
+          url: `${process.env.NEXT_PUBLIC_PRODUCTION_ROOT_URL}`,
+          description: `Documentation for ${process.env.NEXT_PUBLIC_PROJECT_NAME}`,
+          type: "article",
+          images: [
+            {
+              url: `${process.env.NEXT_PUBLIC_PRODUCTION_ROOT_URL}/static/logo.svg`,
+              width: 800,
+              height: 600,
+              alt: "Logo",
+              type: "svg",
+            },
+            {
+              url: `${process.env.NEXT_PUBLIC_PRODUCTION_ROOT_URL}/static/logo.svg`,
+              width: 900,
+              height: 800,
+              alt: "Logo",
+              type: "svg",
+            },
+          ],
+          site_name: `${process.env.NEXT_PUBLIC_PROJECT_NAME}'s Documentation`,
+        }}
+      />
       <SidebarProvider>
         <DarkmodeProvider>
           <Layout>
