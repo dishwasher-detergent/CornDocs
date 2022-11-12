70bd146218a737d8bbf3e92e92de28f4cf7fd8f8
dishwasher-detergent <kenny_bass@outlook.com>
2022-11-12 15:11:47 -0600

    fix????????

---
+++ dist/types/index.ts
@@ -0,0 +1,5 @@
+import { Config } from "./ConfigType";
+import { TypeDocsDetails } from "./TypeDocsDetails";
+import { TypeDocsMetaData } from "./TypeDocsMetadata";
+
+export type { Config, TypeDocsDetails, TypeDocsMetaData };
