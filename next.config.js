09c2cd0b30b7812822f848cf8eb69800fa10e141
dishwasher-detergent <kenny_bass@outlook.com>
2022-11-12 14:45:15 -0600

    remove comments

--- next.config.js
+++ next.config.js
@@ -1,16 +1,2 @@
 const withCorndocs = require("./loader");
 module.exports = withCorndocs();
-
-// const frontmatter = require("remark-frontmatter");
-// const gfm = require("remark-gfm");
-
-// const withMDX = require("@next/mdx")({
-//   extension: /\.mdx?$/,
-//   options: {
-//     remarkPlugins: [frontmatter, gfm],
-//     providerImportSource: "@mdx-js/react",
-//   },
-// });
-// module.exports = withMDX(nextConfig, {
-//   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-// });
