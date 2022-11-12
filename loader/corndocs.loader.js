ee05b4edb6de54f6f08346260140a259880ba45b
dishwasher-detergent <kenny_bass@outlook.com>
2022-11-12 10:05:52 -0600

    update to use webpack loader

---
+++ loader/corndocs.loader.js
@@ -0,0 +1,36 @@
+module.exports = function (source) {
+  let new_content = `
+     import "../styles/globals.css";
+     import "../styles/progress.css";
+     import "../styles/custom.css";
+     import type { AppProps } from "next/app";
+     import { SidebarProvider } from "../context/SidebarContext";
+     import { DarkmodeProvider } from "../context/DarkModeContext";
+     import Layout from "../components/layout/Layout";
+     import { CommandProvider } from "../context/CommandContext";
+
+     function MyApp({ Component, pageProps }: AppProps) {
+       return (
+         <>
+           <CommandProvider>
+             <SidebarProvider>
+               <DarkmodeProvider>
+                 <Layout>
+                   <Component {...pageProps} />
+                 </Layout>
+               </DarkmodeProvider>
+             </SidebarProvider>
+           </CommandProvider>
+         </>
+       );
+     }
+
+     export default MyApp;`;
+
+  const replace_source = source.replace(
+    "<Component {...pageProps} />",
+    new_content
+  );
+
+  return `${new_content}`;
+};
--- next.config.js
+++ next.config.js
@@ -1,28 +1,16 @@
-// /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    loader: "custom",
-    disableStaticImages: true,
-  },
-  webpack: function (config, { isServer }) {
-    if (isServer) {
-      require("./scripts/sitemap-generator");
-    }
-    return config;
-  },
-};
-module.exports = nextConfig;
+const withCorndocs = require("./loader");
+module.exports = withCorndocs();
 
-const frontmatter = require("remark-frontmatter");
-const gfm = require("remark-gfm");
+// const frontmatter = require("remark-frontmatter");
+// const gfm = require("remark-gfm");
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [frontmatter, gfm],
-    providerImportSource: "@mdx-js/react",
-  },
-});
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-});
+// const withMDX = require("@next/mdx")({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [frontmatter, gfm],
+//     providerImportSource: "@mdx-js/react",
+//   },
+// });
+// module.exports = withMDX(nextConfig, {
+//   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+// });
