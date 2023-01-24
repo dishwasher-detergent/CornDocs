07f12498fd811883fb629dceb7895fb20097dcd4
dishwasher-detergent <kenny_bass@outlook.com>
2023-01-23 22:14:07 -0600

    fix styling issue again

--- ui/layout/sidebar/Content.tsx
+++ ui/layout/sidebar/Content.tsx
@@ -39,7 +39,7 @@ export default function Content({ folders, children }: ContentProps) {
       <Collapsible.Root open={open} onOpenChange={setOpen}>
         <Collapsible.Trigger
           className={`${
-            open ? "bg-slate-100" : ""
+            open ? "bg-slate-100 dark:bg-slate-800" : ""
           } flex w-full cursor-pointer flex-row flex-nowrap items-center gap-2 truncate rounded-xl px-3 py-1.5 font-bold hover:bg-slate-100 dark:hover:bg-slate-800`}
         >
           <a
