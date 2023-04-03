05d8db22b8368631d557472405df79772570a955
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-04-03 15:56:35 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.pexels.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
