56032263b7f7e5f5ebaebc94043c75d1f49594d7
Kenneth Bass <kenny_bass@outlook.com>
2024-03-02 08:45:23 -0600

    update packages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link"
 
-import { siteConfig } from "@/config/site.config"
 import { Button } from "@/components/ui/button"
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { siteConfig } from "@/config/site.config"
 
 export default function Main() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="grid grid-cols-3 gap-4">
-        {siteConfig.sections.map((section) => {
+        {siteConfig.sections.map((section, index) => {
           return (
-            <Card className="">
+            <Card key={index}>
               <CardHeader>
                 <CardTitle>{section.title}</CardTitle>
               </CardHeader>
